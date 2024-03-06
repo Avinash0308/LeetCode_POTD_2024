6ae3e07d54f39f863eede4386ecaf75e542d0159
Avinash Agrawal <avinash03082003@gmail.com>
2024-03-06 12:32:00 +0530

    Added 06 March POTD

---
+++ 06_March_Linked_List_Cycle_Solution.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        if (!head || !head->next)
+            return false;
+        ListNode *slow = head, *fast = head->next;
+        while (fast && fast->next)
+        {
+            if (slow == fast)
+                return true;
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return false;
+    }
+};
+int main()
+{
+    return 0;
+}
