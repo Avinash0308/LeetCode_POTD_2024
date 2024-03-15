026f5926106f3487b9c8894983388dac64baf583
Avinash Agrawal <avinash03082003@gmail.com>
2024-03-15 23:23:34 +0530

    Added 15 March POTD

---
+++ 15_March_Product_Of_Array_Except_Self_Solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        int left = 1, right = 1;
+        for (int i = 0; i < n; i++)
+        {
+            ans[i] *= left;
+            ans[n - i - 1] *= right;
+            left *= nums[i];
+            right *= nums[n - i - 1];
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    return 0;
+}
