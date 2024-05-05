efe4e8e08e19bc6ee3f45c61e8edbaefcfd5b8be
Avinash Agrawal <avinash03082003@gmail.com>
2024-05-05 11:36:57 +0530

    Added 06 May POTD

---
+++ 06_May_Delete_Node_In_A_Linked_List_Solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.*/
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
+        while (node->next->next)
+        {
+            node->val = node->next->val;
+            node = node->next;
+        }
+        node->val = node->next->val;
+        node->next = NULL;
+    }
+};
+
+int main()
+{
+    return 0;
+}
