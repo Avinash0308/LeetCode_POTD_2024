d09ad14f59a8597e516998f2761cefe14e64648c
Avinash Agrawal <avinash03082003@gmail.com>
2024-05-07 15:36:41 +0530

    Added 07 May POTD

---
+++ 07_May_Double_A_Number_Represented_As_A_Linked_List_Solution.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* doubleIt(ListNode* head) {
+        std::pair<int, ListNode*> result = func(head);
+        if (result.first!=0) {
+            ListNode* new_head = new ListNode(result.first);
+            new_head->next = result.second;
+            return new_head;
+        }
+        return result.second;
+    }
+    
+private:
+    std::pair<int, ListNode*> func(ListNode* head) {
+        
+        int a = head->val * 2;
+        if (head->next) {
+            std::pair<int, ListNode*> next_result = func(head->next);
+            a += next_result.first;
+        }
+        head->val = a % 10;
+        return {a / 10, head};
+    }
+};
+
+int main(){
+    return 0;
+}
