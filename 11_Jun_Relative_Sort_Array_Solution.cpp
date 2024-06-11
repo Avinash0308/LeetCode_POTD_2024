6b9b434259f338052df85f89f8646b97565567a7
Avinash Agrawal <avinash03082003@gmail.com>
2024-06-11 12:56:48 +0530

    Added 11 Jun potd

---
+++ 11_Jun_Relative_Sort_Array_Solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> relativeSortArray(vector<int> &arr1, vector<int> &arr2)
+    {
+        vector<int> count(1001);
+        for (int i = 0; i < arr1.size(); i++)
+        {
+            count[arr1[i]]++;
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < arr2.size(); i++)
+        {
+            while (count[arr2[i]]--)
+            {
+                ans.push_back(arr2[i]);
+            }
+        }
+
+        for (int i = 0; i < count.size(); i++)
+        {
+            while (count[i]-- > 0)
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    return 0;
+}
