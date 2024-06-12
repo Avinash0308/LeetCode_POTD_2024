bd944dfd593ed6de25eaedfddf2823f1bdac954e
Avinash Agrawal <avinash03082003@gmail.com>
2024-06-12 11:05:17 +0530

    Added 12 Jun POTD

---
+++ 12_Jun_Sort_Colors_Solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int a = 0, b = 0, c = nums.size() - 1;
+        while (b <= c)
+        {
+            if (nums[b] == 0)
+            {
+                swap(nums[b], nums[a]);
+                a++, b++;
+            }
+            else if (nums[b] == 1)
+            {
+                b++;
+            }
+            else
+            {
+                swap(nums[b], nums[c]);
+                c--;
+            }
+        }
+    }
+};
+
+int main()
+{
+    return 0;
+}
