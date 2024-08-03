498ef6c14c95f64e5aee3edba615a31bb77c9f3e
Avinash Agrawal <avinash03082003@gmail.com>
2024-08-03 10:29:32 +0530

    Added 03 Aug POTD

---
+++ 03_Aug_Make_Two_Array_Equal_By_Reversing_Subarray_Solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool canBeEqual(vector<int> &target, vector<int> &arr)
+    {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < target.size(); i++)
+        {
+            mp[target[i]]++;
+            mp[arr[i]]--;
+        }
+        for (auto i : mp)
+        {
+            if (i.second != 0)
+                return false;
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    return 0;
+}
