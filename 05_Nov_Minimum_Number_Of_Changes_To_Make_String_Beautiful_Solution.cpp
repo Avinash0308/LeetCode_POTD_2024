b024c6a6379708fa158237229d8800abf9ffe245
Avinash Agrawal <115834175+Avinash0308@users.noreply.github.com>
2024-11-05 21:36:35 +0530

    Added 05 Nov POTD

---
+++ 05_Nov_Minimum_Number_Of_Changes_To_Make_String_Beautiful_Solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minChanges(string s)
+    {
+        int ans = 0;
+        for (int i = 0; i < s.size(); i += 2)
+        {
+            ans += (s[i] != s[i + 1]);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    return 0;
+}
