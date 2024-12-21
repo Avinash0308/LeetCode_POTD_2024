318ce34fac8229638fcc4877cc3b0caa567bf092
Avinash Agrawal <115834175+Avinash0308@users.noreply.github.com>
2024-12-21 11:18:55 +0530

    Added 21 Dec POTD

--- 20_Dec_Reverse_Odd_Levels_Of_Binary_Tree_Solution.cpp
+++ 20_Dec_Reverse_Odd_Levels_Of_Binary_Tree_Solution.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+ * Definition for a binary tree node. */
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
 class Solution {
 public:
     TreeNode* reverseOddLevels(TreeNode* root) {
